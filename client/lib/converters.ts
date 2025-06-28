c2e02bf00c11f098c8cf6dd6894549cf0c67bd19
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-06-28 15:58:32 +0530

    feat: implement TokenAddRecordDialog and TokenCardRecords components; add error handling utility and site configuration functions

--- client/lib/converters.ts
+++ client/lib/converters.ts
@@ -1,11 +1,12 @@
 export function addressToShortAddress(
-  address: string | undefined
-): string | undefined {
-  let shortAddress = address;
-  if (address && address.length > 10) {
-    shortAddress = `${address.substring(0, 6)}...${address.substring(
-      address.length - 4
-    )}`;
+    address: string | undefined
+  ): string | undefined {
+    let shortAddress = address;
+    if (address && address.length > 10) {
+      shortAddress = `${address.substring(0, 6)}...${address.substring(
+        address.length - 4
+      )}`;
+    }
+    return shortAddress?.toLowerCase();
   }
-  return shortAddress?.toLowerCase();
-}
+  
